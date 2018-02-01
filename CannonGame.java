c8ad18dac060c7f57713cc03ae6894405270d7d3
wolfganglandes <WolfgangLandes@gmx.de>
2018-02-01 15:36:59 +0100

    Add files via upload

--- CannonGame.java
+++ CannonGame.java
@@ -1,7 +1,6 @@
 package de.tuberlin.sese.swtpp.gameserver.model.cannon;
 
 import java.io.Serializable;
-import java.util.*;
 
 import de.tuberlin.sese.swtpp.gameserver.model.Game;
 import de.tuberlin.sese.swtpp.gameserver.model.Move;
@@ -31,9 +30,6 @@ public class CannonGame extends Game implements Serializable{
 
 	// internal representation of the game state
 	// TODO: insert additional game data here
-	
-	char[][] board = new char[10][10];
-	HashMap<Character,Integer> hmap = new HashMap<Character,Integer>();
 
 	
 	/************************
@@ -44,9 +40,6 @@ public class CannonGame extends Game implements Serializable{
 		super();
 
 		// TODO: add further initializations if necessary
-		hmap.put('a',0);hmap.put('b',1);hmap.put('c',2);hmap.put('d',3);hmap.put('e',4);
-		hmap.put('f',5);hmap.put('g',6);hmap.put('h',7);hmap.put('i',8);hmap.put('j',9);
-		setBoard("/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w///b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/");
 		
 	}
 	
@@ -212,389 +205,21 @@ public class CannonGame extends Game implements Serializable{
 	
 	@Override
 	public void setBoard(String state) {
-		char[] stateArray;
-		stateArray = state.toCharArray();
-		
-		int row = 9;
-		int slot = 0;
-		for(int i =0; i< stateArray.length; i++){
-			if(stateArray[i]=='/'){
-				row--;
-				slot=0;
-			}
-			else{
-				if(stateArray[i] >= '1' && stateArray[i]<= '9'){
-					for(int x = 0; x < Character.getNumericValue(stateArray[i]);x++){
-						board[row][slot]='1';
-						slot = slot + 1;
-					}
-					
-				}else{
-					
-				board[row][slot]=stateArray[i];
-				slot++;
-				}
-			}
-		}
-		// for case // empty row, fill with '1's.
-		for(int i = 0 ; i<board.length; i++){
-			for(int x = 0; x<board[i].length; x++){
-				if(board[i][x]==0){board[i][x]='1';}
-			}
-		}
-		
-		
+
+		//TODO: implement
 	}
 	
 	@Override
 	public String getBoard() {
-		String s = "";
-		for(int i = board.length-1 ; i>=0; i--){
-		int count = 0;
-			for(int x = 0; x<board.length; x++){
-				//s = s + board[i][x];
-				if(board[i][x] != '1'){
-					if(count!=0){
-						s=s+(char)(count + '0');
-						count = 0;
-					}
-				s = s + board[i][x];
-				}
-				else{count++;}
-			}
-			if(count!=0 && count !=10){
-				s=s+(char)(count + '0');
-			}
-			if(i!=0){
-					s=s+'/';
-			}
-		}
-		
+
 		//TODO: replace with real implementation
-		return s;
+		return "";
 	}
-	
-	private boolean checkInput(String moveString){
-		if(!(hmap.containsKey(moveString.charAt(0)))||
-				(!Character.isDigit(moveString.charAt(1)) &&
-						Character.getNumericValue(moveString.charAt(1))>=0) ||
-			!(hmap.containsKey(moveString.charAt(3)))||
-				(!Character.isDigit(moveString.charAt(4))  &&
-						Character.getNumericValue(moveString.charAt(4))>=0))
-				{return false;}else{return true;}
-	}
-	
-	private boolean checkCorrectPlayer(char startLocation, Player player){
-			if(((startLocation=='w'||startLocation=='W')&&this.whitePlayer==player)
-					||((startLocation=='b'||startLocation=='B') && this.blackPlayer==player)){
-					return true;
-					}else{return false;}
-	}
-	
-	private boolean checkBasicMove( int start1, int start2, int goal1,int goal2
- ){		char startLocation = board[start1][start2];
- 		char goalLocation = board[goal1][goal2];
-			//Test Basic Move White
-			if(startLocation == 'w' && goalLocation == '1'&& (start1-1 == goal1 && (start2== goal2 || start2 == goal2-1 || start2 == goal2+1))){
-				return true;
-			//Test Basic Move Black
-			}else if(startLocation == 'b' && goalLocation == '1'&&(start1+1 == goal1 && (start2== goal2 || start2 == goal2-1 || start2 == goal2+1))){
-				return true;
-			}else{
-				return false;
-			}
-	}
-	
-	// Retreat
-	private boolean checkRetreatMove( int start1, int start2, int goal1,int goal2){
-		char startLocation = board[start1][start2];
- 		char goalLocation = board[goal1][goal2];
-			//Test Retreat Move black
- 		System.out.println("test1");
- 		
- 		try{if(startLocation == 'b' && goalLocation == '1' && start1 == goal1+2 && ((start2 == goal2 && board[start1-1][start2]=='1') ||( start2 == goal2+2 && board[start1-1][start2-1]=='1') || (start2==goal2-2 && board[start1-1][start2+1]=='1'))){
-			if(board[start1+1][start2]=='w'||board[start1-1][start2]=='w'||board[start1+1][start2+1]=='w'
-			||board[start1][start2+1]=='w'||board[start1-1][start2+1]=='w'||board[start1+1][start2-1]=='w'
-			||board[start1][start2-1]=='w'||board[start1+1][start2-1]=='w'){
-				System.out.println("test2");return true;
-			}
-			//Test Retreat Move white
-		}}catch(IndexOutOfBoundsException e){} 
- 		try{ if(startLocation == 'w' && goalLocation == '1' && start1 == goal1-2 && ((start2 == goal2 && board[start1+1][start2]=='1')|| (start2 == goal2+2 && board[start1+1][start2-1]=='1') || (start2==goal2-2 && board[start1+1][start2+1]=='1'))){
-			if(board[start1+1][start2]=='b'||board[start1-1][start2]=='b'||board[start1+1][start2+1]=='b'
-			||board[start1][start2+1]=='b'||board[start1-1][start2+1]=='b'||board[start1+1][start2-1]=='b'
-			||board[start1][start2-1]=='b'||board[start1+1][start2-1]=='b'){
-				System.out.println("test3");return true;
-			}
-		}}catch(IndexOutOfBoundsException e){} 
-		return false;
-	}
-	
-	private boolean checkCannonMoveWhite( int start1, int start2, int goal1,int goal2){
-		char startLocation = board[start1][start2];
- 		char goalLocation = board[goal1][goal2];
- 		
-		if(startLocation == 'w'&&goalLocation == '1'){
-			try{ if( start1==goal1 && start2+3==goal2 && board[start1][start2+1]=='w' && board[start1][start2+2]=='w'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			try{ if (start1==goal1 && start2-3==goal2 &&  board[start1][start2-1]=='w' && board[start1][start2-2]=='w'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			try{ if(start1==goal1-3 && start2==goal2 &&  board[start1+1][start2]=='w' && board[start1+2][start2]=='w'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			try{ if(start1==goal1+3 && start2==goal2 &&  board[start1-1][start2]=='w' && board[start1-2][start2]=='w'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			try{ if(start1==goal1+3 && start2==goal2-3 && board[start1-1][start2+1]=='w' && board[start1-2][start2+2]=='w'){
-				return true;}}catch(IndexOutOfBoundsException e){}
-			try{ if(start1==goal1+3 && start2==goal2+3 && board[start1-1][start2-1]=='w' && board[start1-2][start2-2]=='w'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			try{ if(start1==goal1-3 && start2==goal2-3 && board[start1+1][start2+1]=='w' && board[start1+2][start2+2]=='w'){
-				return true;}}catch(IndexOutOfBoundsException e){}
-			try{if(start1==goal1-3 && start2==goal2+3 && board[start1+1][start2-1]=='w' && board[start1+2][start2-2]=='w'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			}
-		return false;}
-	
-	private boolean checkCannonMoveBlack( int start1, int start2, int goal1,int goal2){
-		char startLocation = board[start1][start2];
- 		char goalLocation = board[goal1][goal2];
- 		
-		if(startLocation == 'b' && goalLocation == '1'){
-			try{if(start1==goal1 && start2==goal2+3 && board[start1][start2-1]=='b' && board[start1][start2-2]=='b'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{if (start1==goal1 && start2==goal2-3 &&  board[start1][start2+1]=='b' && board[start1][start2+2]=='b'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{ if(start1==goal1+3 && start2==goal2 &&  board[start1-1][start2]=='b' && board[start1-2][start2]=='b'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			try{if(start1==goal1-3 && start2==goal2 &&  board[start1+1][start2]=='b' && board[start1+2][start2]=='b'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{if(start1==goal1+3 && start2==goal2+3 && board[start1-1][start2-1]=='b' && board[start1-2][start2-2]=='b'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{if(start1==goal1+3 && start2==goal2-3 && board[start1-1][start2+1]=='b' && board[start1+2][start2+2]=='b'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{if(start1==goal1-3 && start2==goal2-3 && board[start1+1][start2+1]=='b' && board[start1+2][start2+2]=='b'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{if(start1==goal1-3 && start2==goal2+3 && board[start1+1][start2-1]=='b' && board[start1+2][start2-2]=='b'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-		}
-		return false;
-	}
-	
-	private boolean checkBasicHit( int start1, int start2, int goal1,int goal2){
-		char startLocation = board[start1][start2];
- 		char goalLocation = board[goal1][goal2];
- 		if(startLocation == 'w' && (goalLocation =='b'||goalLocation == 'B')){
- 			if((start1 == goal1+1 && (start2== goal2 || start2 == goal2-1 || start2 == goal2+1))||(start1 == goal1&&(start2==goal2-1 ||start2==goal2+1))){
- 				return true;
-				}
-		}
-		if(startLocation == 'b' && (goalLocation =='w'||goalLocation == 'W')){
-			if((start1 == goal1-1 && (start2== goal2 || start2 == goal2-1 || start2 == goal2+1))||(start1 == goal1&&(start2==goal2-1 ||start2==goal2+1))){
-					return true;
-				}
-		}
-	return false;
-	}
-	
-	private boolean checkCannonShotWhite( int start1, int start2, int goal1,int goal2){
-		char startLocation = board[start1][start2];
- 		char goalLocation = board[goal1][goal2];
- 		
- 		if(startLocation == 'w'&& (goalLocation =='b'||goalLocation == 'B')){
-			try{if(start2==goal2&&start1==goal1+4|| start2==goal2&&start1==goal1+5 && board[start1-1][start2]=='w' && board[start1-2][start2]=='w'&& board[start1-3][start2]=='1'){
-				System.out.println("ASDF");
-				return true; }}catch(IndexOutOfBoundsException e){} 
-			try{ if (start2==goal2 && start1==goal1-4 || start2==goal2 && start1==goal1-5 && board[start1+1][start2+1]=='w' && board[start1+2][start2]=='w'&& board[start1+3][start2]=='1'){
-				return true; }}catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2+4 && start1==goal1 || start2==goal2+5 &&start1==goal1 && board[start1][start2-1]=='w' && board[start1][start2-2]=='w'&& board[start1][start2-3]=='1'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2-4 && start1==goal1 || start2==goal2-5 && start1==goal1 && board[start1][start2+1]=='w' && board[start1][start2+2]=='w'&& board[start1][start2+3]=='1'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2+4 && start1==goal1+4 || start2==goal2+5 && start1==goal1+5 &&  board[start1-1][start2-1]=='w' && board[start1-2][start2-2]=='w'&& board[start1-3][start2-3]=='1'){
-				return true; } }catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2+4 && start1==goal1-4 || start2==goal2+5 && start1==goal1-5 && board[start1+1][start2-1]=='w' && board[start1+2][start2-2]=='w'&& board[start1+3][start2-3]=='1'){
-				return true; } }catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2-4 && start1==goal1-4 || start2==goal2-5 && start1==goal1-5 && board[start1+1][start2+1]=='w' && board[start1+2][start2+2]=='w'&& board[start1+3][start2+3]=='1'){
-				return true; }}catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2-4 && start1==goal1+4 || start2==goal2-5 && start1==goal1+5 && board[start1-1][start2+1]=='w' && board[start1-2][start2+2]=='w'&& board[start1-3][start2+3]=='1'){
-				return true; }}catch(IndexOutOfBoundsException e){} 
-				
-		}
-	return false;
-	}
-	
-	private boolean checkCannonShotBlack( int start1, int start2, int goal1,int goal2){
-		char startLocation = board[start1][start2];
- 		char goalLocation = board[goal1][goal2];
-		if(startLocation == 'b'&& (goalLocation =='w'||goalLocation == 'W')){
-			try{if(start2==goal2&&start1==goal1+4|| start2==goal2&&start1==goal1+5 && board[start1-1][start2]=='b' && board[start1-2][start2]=='b'&& board[start1-3][start2]=='1'){
-				return true;}}catch(IndexOutOfBoundsException e){} 
-			try{ if (start2==goal2 && start1==goal1-4 || start2==goal2&&start1==goal1-5 && board[start1+1][start2]=='b' && board[start1+2][start2]=='b'&& board[start1+3][start2]=='1'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2+4 &&start1==goal1 || start2==goal2+5&&start1==goal1 && board[start1][start2-1]=='b' && board[start1][start2-2]=='b'&& board[start1][start2-3]=='1'){
-				return true; } }catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2-4&&start1==goal1 || start2==goal2-5&&start1==goal1 && board[start1][start2+1]=='b' && board[start1][start2+2]=='b'&& board[start1][start2+3]=='1'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2+4 &&start1==goal1+4 || start2==goal2+5&&start1==goal1+5 &&  board[start1-1][start2-1]=='b' && board[start1-2][start2-2]=='b'&& board[start1-3][start2-3]=='1'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{if(start2==goal2+4&&start1==goal1-4 || start2==goal2+5&&start1==goal1-5 && board[start1+1][start2-1]=='b' && board[start1+2][start2-2]=='b'&& board[start1+3][start2-3]=='1'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2-4 &&start1==goal1-4 || start2==goal2-5&&start1==goal1-5 && board[start1+1][start2+1]=='b' && board[start1+2][start2+2]=='b'&& board[start1+3][start2+3]=='1'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-			try{ if(start2==goal2-4&&start1==goal1+4 || start2==goal2-5&&start1==goal1+5 && board[start1-1][start2+1]=='b' && board[start1-2][start2+2]=='b'&& board[start1-3][start2+3]=='1'){
-				return true;} }catch(IndexOutOfBoundsException e){} 
-		}
-	return false;
-	}
-	
-	private boolean setCity(int start1, int start2, Player player){
-		if(player ==this.whitePlayer){
-			if(start1<9&&start1>0&&start2==9){
-				board[start1][start2]='W';
-				return true;
-			}
-			
-		}else if(player == this.blackPlayer){
-			if(start1<9&&start1>0&&start2==0){
-				board[start1][start2]='B';
-				return true;
-			}
-		}
-		
-			return false;
-	}
-	private boolean checkCity(Player player){
-		if(player==this.whitePlayer){
-			for(int i = 0 ; i<board.length; i++){
-				for(int x = 0; x<board[i].length; x++){
-					if(board[i][x]=='W'){return true;}
-				}
-			}
-		}else if(player==this.whitePlayer){
-			for(int i = 0 ; i<board.length; i++){
-				for(int x = 0; x<board[i].length; x++){
-					if(board[i][x]=='B'){return true;}
-				}
-			}
-		}
-		return false;
-	}
-	private boolean move(int start1, int start2, int goal1,int goal2){
-		board[goal1][goal2]=board[start1][start2];
-		board[start1][start2]='1';
-		return true;
-	}
-	private boolean kill(int goal1,int goal2, Player player){
-		if((player==this.whitePlayer &&board[goal1][goal2]=='B')
-			||(player==this.blackPlayer &&board[goal1][goal2]=='W')){
-			player.isWinner();
-			this.finished=true;
-			return true;
-		}
-		
-		board[goal1][goal2]='1';
-		return false;
-	}
-
-	private boolean checkMoveLeft(Player player){
-			for(int i = 0 ; i<board.length; i++){
-				for(int x = 0; x<board[i].length; x++){
-					if((board[i][x]=='w'&&player==this.whitePlayer)||(board[i][x]=='b'&&player==this.blackPlayer)){
-						for(int ii = 0 ; ii<board.length; ii++){
-							for(int xx = 0; xx<board[ii].length; xx++){
-								if(checkMoves(i,x,ii,xx)||
-								checkCannonShotWhite( i,x,ii,xx)
-								||checkCannonShotBlack( i,x,ii,xx )||
-								checkBasicHit( i,x,ii,xx )){return true;}
-							}
-						}
-					}
-				}
-			}
-		return false;
-	}
-	
-	private boolean checkMoves ( int start1, int start2, int goal1,int goal2){
-		
-		if(checkBasicMove( start1, start2,  goal1, goal2 )){
-			return true;
-		}
-		
-		if(checkRetreatMove( start1, start2,  goal1, goal2 )){
-			return true;
-		}
-		System.out.println("I move");
-		if(checkCannonMoveWhite(start1, start2,  goal1, goal2 )
-			||checkCannonMoveBlack( start1, start2,  goal1, goal2 )){
-			return true;
-		}
-		System.out.println("I move");
-		return false;
-	}
-
 	
 	@Override
 	public boolean tryMove(String moveString, Player player) {
-		//CHECK IF MOVE COMMAND ON BOARD!
-		if(!checkInput(moveString)){return false;}
-		//Example move: "h6-h5"
-		int start1 =Character.getNumericValue(moveString.charAt(1));
-		int start2 = hmap.get(moveString.charAt(0));
-		int goal1= Character.getNumericValue(moveString.charAt(4));
-		int goal2 = hmap.get(moveString.charAt(3));
-		char startLocation = board[start1][start2];
- 		char goalLocation = board[goal1][goal2];
- 		System.out.println("test");
- 		//NOTHING or WRONG color selected
- 		if(!checkCorrectPlayer(startLocation, player)){
- 			return false;
- 		}
- 		System.out.println("test");
- 		//SET CITY
-		if(startLocation == goalLocation){
-			//Set city
-			if(!checkCity(player)){
-				System.out.println("I build City");
-				return setCity(start1, start2, player);
-			}else return false;
-		}
-		System.out.println("test");
-		if(checkMoves(start1, start2,  goal1, goal2)){
-			System.out.println("I move");
-			move(start1, start2,  goal1, goal2);
-			updateNext();
-			if(!checkMoveLeft(this.nextPlayer)){
-				player.isWinner();
-				this.finished=true;
-				return true;
-			}else{return true;}
-			
-		}else if(checkCannonShotWhite( start1, start2,  goal1, goal2 )
-				||checkCannonShotBlack( start1, start2,  goal1, goal2 )){
-			System.out.println("I shoot Cannon");
-			if(kill( goal1, goal2,player)){return true;}
-			updateNext();
-			return true;
-//			if(!checkMoveLeft(this.nextPlayer)){
-//				player.isWinner();
-//				this.finished=true;
-//				return true;
-//			}else{return true;}
-			
-		}else if(checkBasicHit( start1, start2,  goal1, goal2 )){
-			System.out.println("I Hit");
-			if(kill( goal1, goal2,player)){return true;}
-			move(start1, start2,  goal1, goal2);
-			
-			updateNext();
-			if(!checkMoveLeft(this.nextPlayer)){
-				player.isWinner();
-				this.finished=true;
-				return true;
-		}else{return true;}
-		}
-		System.out.println("false return ");
+		//TODO: implement
 		return false;
-		
 	}
 	
 
